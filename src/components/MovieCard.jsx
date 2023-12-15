8b1cf8a12f71af05b32002d46a6668c11a3b6889
mmwandera <mmwandera74@gmail.com>
2023-12-15 16:20:03 +0300

    added tab design and fetched data for each tabs

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import './style.css';
 
-function MovieCard ({movie}) {
-    return(
-        <div className="movie-card">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Movie Poster for ${movie.title}`} />
-            <h3>{movie.title}</h3>
-            <h5>Release Date:</h5>
-            <p>{movie.release_date}</p>
-            <h5>Rating:</h5>
-            <p>{movie.vote_average}</p>
-        </div>
-    )
+function MovieCard({ movie }) {
+  // Round off the rating to one decimal place
+  const roundedRating = Math.round(movie.vote_average * 10) / 10;
+
+  // Determine the color of the chip based on the rating range
+  let chipColor;
+  if (roundedRating > 7) {
+    chipColor = 'green';
+  } else if (roundedRating >= 6 && roundedRating <= 6.9) {
+    chipColor = 'yellow';
+  } else {
+    chipColor = 'red';
+  }
+
+  return (
+    <div className="movie-card">
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Movie Poster for ${movie.title}`} />
+      <h3 style={{ color: '#00A9FF', marginBottom: '5px' }}>{movie.title}</h3>
+      <p style={{ fontSize: '14px', marginBottom: '5px', display: 'flex', alignItems: 'center' }}>
+        <span style={{ marginRight: '5px', fontWeight: 'bold' }}>Rating:</span> 
+        <span className={`rating-chip ${chipColor}`}>{roundedRating}</span>
+      </p>
+      <p style={{ fontSize: '14px', marginBottom: '5px' }}>
+        <span style={{ marginRight: '5px', fontWeight: 'bold' }}>Release Date:</span>
+        <span style={{ marginLeft: '5px' }}>{movie.release_date}</span>
+      </p>
+    </div>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -3,34 +3,54 @@ import MovieCard from './MovieCard';
 import './style.css';
 
 function Tabs() {
+  const [movies, setMovies] = useState([]);
+  const [activeTab, setActiveTab] = useState('popular');
 
-    const [movies, setMovies] = useState([]);
-    
-    const APIURL = 'https://api.themoviedb.org/3/';
-    const APIKEY = '8cc73deace87d0030f1c7700b02bbbcc';
+  const APIKEY = '8cc73deace87d0030f1c7700b02bbbcc';
+  const APIURL = 'https://api.themoviedb.org/3/';
 
-    useEffect(() => {
-        fetch(`${APIURL}movie/popular?api_key=${APIKEY}&language=en-US&page=1`)
-            .then(response => response.json())
-            .then(data =>{
-                // console.log(data);
-                setMovies(data.results);
+  useEffect(() => {
+    fetchMovies(activeTab);
+  }, [activeTab]);
 
-            })
-    })
+  const fetchMovies = (tab) => {
+    let url;
 
-    return (
-        <section className="tabs-section">
-            <div className="tab-buttons">
-                <button>Popular</button>
-                <button>Top Rated</button>
-                <button>Upcoming</button>
-            </div>
-            <div className="movie-cards">
-                {movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
-            </div>
-        </section>
-    );
+    switch (tab) {
+      case 'top_rated':
+        url = `${APIURL}movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`;
+        break;
+      case 'upcoming':
+        url = `${APIURL}movie/upcoming?api_key=${APIKEY}&language=en-US&page=1`;
+        break;
+      default:
+        url = `${APIURL}movie/popular?api_key=${APIKEY}&language=en-US&page=1`;
+        break;
+    }
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setMovies(data.results);
+      });
+  };
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <section className="tabs-section">
+      <div className="tab-buttons">
+        <button className={activeTab === 'popular' ? 'active' : ''} onClick={() => handleTabClick('popular')}>Popular</button>
+        <button className={activeTab === 'top_rated' ? 'active' : ''} onClick={() => handleTabClick('top_rated')}>Top Rated</button>
+        <button className={activeTab === 'upcoming' ? 'active' : ''} onClick={() => handleTabClick('upcoming')}>Upcoming</button>
+      </div>
+      <div className="movie-cards">
+        {movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </section>
+  );
 }
 
 export default Tabs;
